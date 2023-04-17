303e65ba2df7d8ff119719d87826a63f557a9d94
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-04-17 10:09:42 +0600

    job portal created

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ async function run(){
     try {
         await client.connect();
         const postCollection = client.db('Education_Management_System').collection('posts');
+        const jobPostCollection = client.db('Education_Management_System').collection('jobs');
 
         app.get('/posts', async(req, res)=>{
-            const query = {};
-            const cursor =  postCollection.find(query);
+            // const query = {};
+            const cursor =  postCollection.find({});
             const posts = await cursor.toArray();
             res.send(posts);
         })
@@ -26,6 +27,20 @@ async function run(){
         app.post('/posts', async(req, res)=>{
             const announce = req.body;
             const result = await postCollection.insertOne(announce);
+            return res.send(result);
+        })
+
+        
+        app.get('/jobs', async(req, res)=>{
+            // const query = {};
+            const cursor =  jobPostCollection.find({});
+            const jobs = await cursor.toArray();
+            res.send(jobs);
+        })
+        
+        app.post('/jobs', async(req, res)=>{
+            const jobs = req.body;
+            const result = await jobPostCollection.insertOne(jobs);
             return res.send(result);
         })
     } 
